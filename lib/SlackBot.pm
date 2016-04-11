3882a65672b6bbde94be3a1a0d280917d2837140
v-zhuravlev <v-zhuravlev@users.noreply.github.com>
2016-04-11 10:46:34 +0300

    Added sub to prepare Zabbix macros to be proper JSON String

--- lib/SlackBot.pm
+++ lib/SlackBot.pm
@@ -1,7 +1,7 @@
 package SlackBot;
 use strict;
 use warnings;
-our $VERSION = '0.3';
+our $VERSION = '0.4';
 
 use LWP;
 use URI;
@@ -351,7 +351,10 @@ sub create_json_if_plain {
     my $json_hash;
     my $json_attach;
     eval {    #check if already JSON:
-        $json_hash = JSON::XS->new->decode( $contents->{message} );
+        
+        my $message = zbx_macro_to_json($contents->{message});
+        $json_hash = JSON::XS->new->decode( $message );
+
     };
     if ($@) {
         print "message is not JSON, going to proceed as with regular text\n";
@@ -386,6 +389,44 @@ sub create_json_attach_only {
         ]
     );
 }
+
+
+=zbx_macro_to_json
+transform everything(zabbix macros) in double squares [[ ]] to json STRING
+note that unicode chars already encoded to \u1234 currently in message when called.
+=cut
+
+sub zbx_macro_to_json {
+     my $message = shift;
+     my $orig ;
+     my $result ;
+            
+        while (  $message =~ /( \[ \[) (.*?) (\] \] ) /gxs) {
+            
+            $orig = $1.$2.$3;
+            $result = $2;
+            print $orig."\n";
+            #order matters
+            utf8::encode( $result);
+            $result =~  s/ \\ /\\\\/xg; #\
+            $result =~  s/ \/ /\\\//xg; #/
+            $result =~  s/ " /\\"/xg; #"
+            $result =~  s/ \n /\\n/xg; #\n
+            $result =~  s/ \r /\\r/xg; #\r
+            $result =~  s/ \x{8} //xg; #backspace
+            $result =~  s/ \x{C} //xg; #formfeed
+            $result =~  s/ \x{9} /    /xg; #horizontal tab
+            utf8::decode( $result);
+                        
+            $orig = quotemeta($orig);
+            
+            $message =~ s/$orig/$result/;
+        }
+        print $message."\n";
+        return $message;
+
+}
+
 
 sub get_with_retries {
     my $self          = shift;
